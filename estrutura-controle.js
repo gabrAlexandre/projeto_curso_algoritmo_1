5b741684f46b5dd00597ffb8e56c5f3a64a56b74
gabrAlexandre <gr03107@gmail.com>
2020-09-07 21:29:11 -0300

    Removidos os comentarios do font em portugol

--- estrutura-controle.js
+++ estrutura-controle.js
@@ -1,20 +1,3 @@
-/*escreval(" ")
- leia(nome)
- 
-escreval("Digite a primeira nota do aluno: ")
- leia(nota1)
- 
-escreval(" ")
- leia(nota2)
- 
-media := (nota1 + nota2) / 2
-
-if media >= 5 entao
-  escreval(nome, " está aprovado(a)! ")
- ifnao
-  escreval(nome, " está reprovado(a)! ")
-fimif*/
-
 var nome, nota01, nota02;
 nome = prompt("Digite o nome do aluno:")
 nota01 = prompt("Digite a primeira nota do aluno:")
--- opr-relacionais.js
+++ opr-relacionais.js
@@ -1,19 +1,3 @@
-/*
-Var
-// ifção de Declarações das variáveis 
- nota01, nota02: real
-
-Inicio
-// ifção de Comandos, procedimento, funções, operadores, etc... 
- nota01 := 10
- nota02 := 15
- 
-if(nota01 <> nota02)entao
- escreval("Nota01 é diferente")
-ifnao
- escreval("Nota01 é igual")
-fimif
-*/
 var nota01, nota02;
 
 if(nota01 != nota02)
